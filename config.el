cf5c4e152347cc550192937e01d44da1b7f912a5
Vaibhav Pujari <276vaibhav+github@gmail.com>
2020-03-30 17:42:13 +0530

    Minor customizations

--- config.el
+++ config.el
@@ -19,16 +19,19 @@
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
 ;; test
-(setq doom-font (font-spec :family "monospace" :size 17)
+(setq doom-font (font-spec :family "Meslo LG S DZ" :size 23)
       doom-variable-pitch-font (font-spec :family "sans"))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. These are the defaults.
-(setq doom-theme 'doom-solarized-light)
+(setq doom-theme 'doom-nord-light)
 
 ;; If you intend to use org, it is recommended you change this!
 (setq org-directory "~/org/")
+
+;; Custom bullets
+(setq org-bullets-bullet-list '("⍟" "★" "➛" "。"))
 
 ;; If you want to change the style of line numbers, change this to `relative' or
 ;; `nil' to disable it:
